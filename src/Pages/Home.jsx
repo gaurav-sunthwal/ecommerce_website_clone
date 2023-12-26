11fc9e4c5078ca28c8ade59b6db86d63f9cfe158
Gaurav Naresh Sunthwal <70788155+codewithgaurav252005@users.noreply.github.com>
2023-12-26 21:16:09 +0530

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,10 @@
 import "../Styling/Home.css";
-import React, { useState } from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const ImageSlider = ({ images }) => {
-  const [currentImage, setCurrentImage] = useState(0);
-
-  const handleImageChange = (index) => {
-    setCurrentImage(index);
-  };
-};
 function Home() {
   return (
-    <div>
-      <Carousel
-        showThumbs={false}
-        selectedItem={currentImage}
-        onChange={handleImageChange}
-      >
-        {images.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt={`Slide ${index}`} />
-          </div>
-        ))}
-      </Carousel>
-    </div>
+    <>
+        <h1>Code</h1>
+    </>
   );
 }
 
