57151a13a9149ef3092b007974c605196a25105e
Gaurav Naresh Sunthwal <70788155+codewithgaurav252005@users.noreply.github.com>
2023-12-30 21:03:03 +0530

    push

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -19,17 +19,34 @@ import { Button } from "@chakra-ui/react";
 import React, { useState } from "react";
 function Navbar() {
   const [isVisible, setIsVisible] = useState(false);
-  const [inputFormType, setinputFormType] = useState(true);
+  const [emailinputFormType, setemailinputFormType] = useState(true);
+  const [passwordinputFormType, setpasswordinputFormType] = useState(true);
   const [inputValue, setInputValue] = useState(true);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   function toggleItsVisiblity() {
     setIsVisible(!isVisible);
   }
-  const handleInputChange = (event) => {
+  const handleEmailInputChange = (event) => {
     // Update the state with the new input value
-    setInputValue(event.target.value);
+    setemailinputFormType(event.target.value);
   };
+  const handlePasswordInputChange = (event) => {
+    // Update the state with the new input value
+    setpasswordinputFormType(event.target.value);
+  };
+  const handleClick = (event) => {
+    if(emailinputFormType === "garavsunthwal22005@gmail.com" && passwordinputFormType === "gaurav22005"){
+      alert("Congratulations")
+    }
+    else{
+      alert("No ")
+      emailinputFormType("")
+      passwordinputFormType("")
+    }
+    // Update the state with the new input value
+  };
+  
   function submitForm() {
     //    alert("Welcome Back!")
     alert(`Input value is: ${inputValue}`);
@@ -98,7 +115,7 @@ function Navbar() {
               </Drawer>
             </div>
             <div className="userLogin">
-            <FaRegUser
+              <FaRegUser
                 ref={btnRef}
                 background={"transparent"}
                 color={"white"}
@@ -116,7 +133,11 @@ function Navbar() {
                   <DrawerHeader>Create your account</DrawerHeader>
 
                   <DrawerBody>
-                    <Input placeholder="Type here..." />
+                    <form>
+                      <Input onChange={handleEmailInputChange} type="email" placeholder="Enter Email..." />
+                      <Input onChange={handlePasswordInputChange} type="password" placeholder="Password" marginTop={"12px"} />
+                      <Button type="submit" onClick={handleClick} colorScheme="blue" p={"12px"} marginTop={"12px"}>Submit</Button>
+                    </form>
                   </DrawerBody>
 
                   <DrawerFooter>
